f62e5837b21f09c700e518430063f33ee46e3ecc
nikhil <nikhilbhagat2293@gmail.com>
2023-06-10 17:11:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// console.log("Hello world");  Question 1
+
+// Question 2
+
+// let a = prompt("enter a");
+// let b = prompt("enter b");
+// let c = prompt("enter c");
+
+// const result = b * b - 4 * a * c;
+// console.log(result);
+
+// let a = prompt("enter a");
+// let b = prompt("enter b");
+// let c = prompt("enter c");
+
+// Question 3
+
+//   //   let b = document.getElementById("b").value;
+//   //   let a = document.getElementById("a").value;
+//   //   let c = document.getElementById("c").value;
+//   //   let res = document.getElementById("result");
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener("click", () => {
+//   console.log("click");
+
+//   //   console.log("clicked");
+//   //   const result = b * b - 4 * a * c;
+//   //   if (result === 0) {
+//   //     res.innerHTML = "real values";
+//   //     console.log("real values");
+//   //   } else if (result < 0) {
+//   //     res.innerHTML = "imaginary values";
+//   //     console.log("imaginary values");
+//   //   }
+// });
+
+// for (let i = 0; i < 4; i++) {
+//   //Row
+//   var a = "";
+
+//   for (let j = 0; j !== i % 2; j + 2) {
+//     //
+
+//     a += `${j + 1} `;
+//   }
+//   console.log(a);
+//   console.log("\n");
+// }
+
+// for (let i = 0; i < 20; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
